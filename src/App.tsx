87bdc7f0448082027b0ab399db97fa72f19279fb
キャラメル <kyaram3ru@gmail.com>
2022-07-28 17:23:08 +0900

    fix emojis

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 import { XMLValidator } from "fast-xml-parser";
 
 const singleEmojiRegex =
-  /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{1FA70}-\u{1FAFF}\u{1f300}-\u{1F5ff}\u{1f90c}-\u{1F9ff}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug;
+  /[\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{1FA70}-\u{1FAFF}\u{1f300}-\u{1F9ff}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/ug;
 
 function fallbackCopyTextToClipboard(text: string) {
   const textArea = document.createElement("textarea");
@@ -52,8 +52,8 @@ function App() {
         `flex(& col) w-full max-w-md rounded-xl overflow-hidden shadow-xl bg-white`}
     >
       <Textarea />
-      <div className={tw`px-4 space-y-1 py-3`}>
-        <h2 className={tw`text-xs font-bold text-gray-500`}>結果</h2>
+      <div className={tw`px-4 space-y-1 py-3 text-white bg-gray-600`}>
+        <h2 className={tw`text-xs font-bold text-gray-300`}>結果</h2>
         <div
           id="__preview"
           className={tw`w-full break-all`}
@@ -226,6 +226,7 @@ const Textarea = () => {
       }`;
 
       code.innerText = newCode;
+      (window as any).twemoji.parse(preview);
     }
   }
 
